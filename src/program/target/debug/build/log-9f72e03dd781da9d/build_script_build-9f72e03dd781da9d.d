d7b1f41181d4bf108880203bb5ed890a8fc89eff
Wontae <bbrick@Wontae-3.local>
2022-08-24 13:34:58 +0900

    fixed untracked files

--- src/program/target/debug/build/log-9f72e03dd781da9d/build_script_build-9f72e03dd781da9d.d
+++
@@ -1,5 +0,0 @@
-/Users/bbrick/Workspace/SOLANA/con1/target/debug/build/log-9f72e03dd781da9d/build_script_build-9f72e03dd781da9d: /Users/bbrick/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/build.rs
-
-/Users/bbrick/Workspace/SOLANA/con1/target/debug/build/log-9f72e03dd781da9d/build_script_build-9f72e03dd781da9d.d: /Users/bbrick/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/build.rs
-
-/Users/bbrick/.cargo/registry/src/github.com-1ecc6299db9ec823/log-0.4.17/build.rs:
